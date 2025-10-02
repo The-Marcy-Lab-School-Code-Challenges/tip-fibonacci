e34302a15a139bd79a3d64bb5dc8b1480e3460c2
Carmen Salas <carmensalas0114@gmail.com>
2025-10-02 12:44:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-function fizzBuzz(){
-  
+// Prints the first n Fibonacci numbers
+const printFibonacci = (n) => {
+ 
 }
 
-
-function fizzBuzzN(n){
-  
-}
-
-
-function fizzBuzzArray(number){
-  
-}
-
-
+printFibonacci(5);  // should print 0, 1, 1, 2, 3
+printFibonacci(10); // should print 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
